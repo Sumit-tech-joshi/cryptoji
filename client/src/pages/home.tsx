63f41673aad7e2cca2cdb5559ab951b3c26cdd51
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-01-28 00:00:23 -0800

    Add coin interface and intigrate coin list api

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -4,10 +4,26 @@ import { Link } from 'react-router-dom';
 import '../App.css';  // Import the global stylesheet
 
 // Coin interface to define structure
-
+interface Coin {
+    id: string;
+    symbol: string;
+    name: string;
+    image: string;
+    current_price: number;
+    market_cap: number;
+    price_change_percentage_24h: number;
+  }
 
 const Home: React.FC = () => {
+    const [coins, setCoins] = useState<Coin[]>([]);
 
+    useEffect(() => {
+      // Fetch coins
+      axios.get('http://localhost:3001/api/coins')
+        .then((res) => setCoins(res.data))
+        .catch((err) => console.error(err));
+  
+    }, []);
 
   return (
     <div className="home-container">
