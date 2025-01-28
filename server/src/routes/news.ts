4e7bf697a0c838e8924b6140bfe0b05ffcafe052
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-01-27 23:44:42 -0800

    Change in news api url

--- server/src/routes/news.ts
+++ server/src/routes/news.ts
@@ -7,7 +7,7 @@ const NEWS_API_KEY = process.env.NEWS_API_KEY || '';
 router.get('/', async (req: any, res: any) => {
   const { q } = req.query; // query param, e.g., coin ID or name
   try {
-    const response = await axios.get('https://cryptopanic.com/api/free/v1/posts/?auth_token=73259e4a53342e54d90af10dbbfa2ce1764d8a53', {
+    const response = await axios.get(`https://newsdata.io/api/1/latest?apikey=pub_668626c590ad16cc0e9756aa212b164fd9761&category=politics&country=ca`, {
 
     });
     return res.json(response.data);
