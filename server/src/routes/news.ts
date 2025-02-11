5b5d6a88eee1a5603c15c7ae44f5643ee6997827
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-02-11 01:09:50 -0800

    Param chnage in news API url

--- server/src/routes/news.ts
+++ server/src/routes/news.ts
@@ -5,9 +5,9 @@ const router = Router();
 const NEWS_API_KEY = process.env.NEWS_API_KEY || '';
 
 router.get('/', async (req: any, res: any) => {
-  const { q } = req.query; // query param, e.g., coin ID or name
+  // const { q } = req?.query?.param || 'cryptocurrency'; // query param, e.g., coin ID or name
   try {
-    const response = await axios.get(`https://newsdata.io/api/1/latest?apikey=pub_668626c590ad16cc0e9756aa212b164fd9761&category=politics&country=ca`, {
+    const response = await axios.get(`https://newsdata.io/api/1/latest?apikey=pub_668626c590ad16cc0e9756aa212b164fd9761&q=cryptocurrency&country=ca`, {
 
     });
     return res.json(response.data);
