237ad3f60a2877c62c2be09a8567dab83059db71
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-02-25 01:16:55 -0800

    Add clerk implementation at index wrapper

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App.tsx';
-import 'react-multi-carousel/lib/styles.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import "react-multi-carousel/lib/styles.css";
+import { ClerkProvider } from "@clerk/clerk-react";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+
+// Need to remove this and use it from env file
+const PUBLISHABLE_KEY =
+  "pk_test_c21pbGluZy1wdW1hLTEzLmNsZXJrLmFjY291bnRzLmRldiQ";
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk Publishable Key to the .env.local file");
+}
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/login">
+      <App />
+    </ClerkProvider>
   </React.StrictMode>
 );
