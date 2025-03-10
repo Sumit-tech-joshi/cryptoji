a6afa26cabbf5525065270d859a72320906064dc
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-03-10 16:01:30 -0700

    Add dynamic feature on hero  for logged in user

--- client/src/components/hero.tsx
+++ client/src/components/hero.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "../App.css";
+import { Link } from "react-router-dom";
 import heroImage from "../assets/hero.png";
 import NavBar from "../components/navbar.tsx"; // Ensure correct import path
+import { useUser } from "@clerk/clerk-react"; // Clerk User Hook
 
 const Hero: React.FC = () => {
+  const { user, isLoaded } = useUser(); // Get Clerk User
+
   const scrollToContent = () => {
     const contentSection = document.getElementById("content-section");
     if (contentSection) {
@@ -17,12 +21,19 @@ const Hero: React.FC = () => {
       <div className="hero-section desktop-width">
         <div className="hero-content">
           <h1>Explore, Analyze & Track Cryptocurrencies</h1>
-          <p>Get real-time price updates, latest news, and AI-powered insights.</p>
+          <p>
+            Get real-time price updates, latest news, and AI-powered insights.
+          </p>
 
           <div className="hero-buttons">
             <button className="btn-primary" onClick={scrollToContent}>
               Start Exploring
             </button>
+            {isLoaded && user && (
+              <Link to="/research" className="nav-btn user-btn btn-primary">
+                Analyze My Coins
+              </Link>
+            )}
           </div>
         </div>
 
