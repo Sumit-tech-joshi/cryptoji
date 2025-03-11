78747c8172665e44f204018d160887ade4ece7dd
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-03-10 21:43:09 -0700

    Add news API intigration with caching

--- server/src/routes/news.ts
+++ server/src/routes/news.ts
@@ -1,18 +1,36 @@
 import { Router } from 'express';
 import axios from 'axios';
+import Cache from '../models/cache';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const router = Router();
 const NEWS_API_KEY = process.env.NEWS_API_KEY || '';
 
 router.get('/', async (req: any, res: any) => {
-  // const { q } = req?.query?.param || 'cryptocurrency'; // query param, e.g., coin ID or name
+  const cacheKey = 'crypto_news'; // Unique key for news data
+  const queryData = req.q || 'cryptocurrency';
   try {
-    const response = await axios.get(`https://newsdata.io/api/1/latest?apikey=pub_668626c590ad16cc0e9756aa212b164fd9761&q=cryptocurrency&country=ca`, {
+    // Check Cache First
+    const cachedData = await Cache.findOne({ key: cacheKey });
 
-    });
+    if (cachedData) {
+      console.log('Serving News from Cache');
+      return res.json(cachedData.data);
+    }
+
+    const url = `https://newsdata.io/api/1/latest?apikey=${NEWS_API_KEY}&q=${queryData}&country=ca`;
+
+    console.log({ url })
+    // Fetch Fresh Data
+    const response = await axios.get(url);
+
+    // Store in Cache
+    await Cache.create({ key: cacheKey, data: response.data });
+
     return res.json(response.data);
   } catch (error) {
-    console.error(error);
+    console.error(' Error fetching news:', error);
     return res.status(500).json({ error: 'Failed to fetch news' });
   }
 });
