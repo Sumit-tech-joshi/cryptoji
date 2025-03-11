088865098ddf8f96ce043c88a259fb78ec85aa3d
sumit joshi <47482270+Sumit-tech-joshi@users.noreply.github.com>
2025-03-10 21:46:18 -0700

    Change chached time to 10 hour at cache scheema

--- server/src/models/cache.ts
+++ server/src/models/cache.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const cacheSchema = new mongoose.Schema({
   key: { type: String, required: true, unique: true }, // Unique key (API Endpoint)
   data: { type: mongoose.Schema.Types.Mixed, required: true }, // Cached Data
-  createdAt: { type: Date, default: Date.now, expires: 3600 } // Auto-delete after 1 hour
+  createdAt: { type: Date, default: Date.now, expires: 3600 * 10 } // Auto-delete after 10 hour
 });
 
 const Cache = mongoose.model('Cache', cacheSchema);
